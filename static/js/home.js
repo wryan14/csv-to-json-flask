9b4f4de9ab4e2e78dec6757a2452fcf4a0a2810f
Ryan Wolfslayer <xsltlibrary7@protonmail.com>
2023-03-05 19:43:15 -0500

    accessible style added

---
+++ static/js/home.js
@@ -0,0 +1,24 @@
+// Handle form submission with AJAX
+document.getElementById("csvForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    document.getElementById("spinner").style.display = "block";
+    document.getElementById("success-message").style.display = "none";
+    document.getElementById("error-message").style.display = "none";
+    let form_data = new FormData(document.getElementById("csvForm"));
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "{{ url_for('views.upload_file') }}", true);
+    xhr.onload = function (e) {
+      document.getElementById("spinner").style.display = "none";
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          document.getElementById("csvForm").reset();
+          document.getElementById("success-message").style.display = "block";
+        } else {
+          document.getElementById("error-message").style.display = "block";
+          console.error(xhr.statusText);
+        }
+      }
+    };
+    xhr.send(form_data);
+  });
+  
