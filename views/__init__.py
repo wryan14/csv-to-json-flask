f72bda817885ddc16b3797890f0e163ed473a270
Ryan Wolfslayer <xsltlibrary7@protonmail.com>
2023-03-15 12:39:59 -0400

    #2 - updated API accidentally was reverted. Updating

--- views/__init__.py
+++ views/__init__.py
@@ -88,29 +88,27 @@ def file_manager():
     return render_template('file_manager.html')
 
 
-@views.route('/api/search')
-def search():
+@views.route('/api/data/search')
+@views.route('/api/data/<filename>/search')
+def search(filename='data'):
     """
     Search API endpoint that returns results from a JSON data file.
-
     Args:
+        filename (str): The filename of the JSON data file (default: 'data').
         q (str): The search query (keyword) to filter the results (default: None).
         field (str): The field(s) to search in, separated by commas. If not provided or empty string, all fields are searched (default: None).
         key (str): The key(s) to filter the results, separated by commas. If not provided or empty string, all items are returned (default: None).
-
     Returns:
         A JSON response containing search results.
-
     Raises:
         None
     """
-    filename = os.path.join(current_app.static_folder, 'data.json')
-    if not os.path.isfile(filename):
-        response = requests.get(url_for('views.list_files', _external=True))
-        response = response.json()['files']
-        response = [x.split('.json')[0] for x in response]
-        return render_template('no-data.html', files=response)
-    with open(filename) as f:
+    filename = f'{filename}.json'
+    filepath = os.path.join(current_app.static_folder, filename)
+    if not os.path.isfile(filepath):
+        error_msg = f'Error: {filename} file not found.'
+        return render_template('no-data.html', error_msg=error_msg)
+    with open(filepath) as f:
         data = json.load(f)
 
     search_query = request.args.get('q', '').strip().lower()
@@ -151,7 +149,6 @@ def search():
                     results.append(item)
 
     return jsonify(results), 200, {'Content-Type': 'application/json'}
-
 
 @views.route('/api-table')
 def data_table():
