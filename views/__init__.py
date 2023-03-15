3c6e6f0d7e6648b55e6a1f8ddded8c6e27cf799c
Ryan Wolfslayer <xsltlibrary7@protonmail.com>
2023-03-15 12:00:06 -0400

    #2 - API endpoint updated to take different collections.

--- views/__init__.py
+++ views/__init__.py
@@ -88,12 +88,14 @@ def file_manager():
     return render_template('file_manager.html')
 
 
-@views.route('/api/search')
-def search():
+@views.route('/api/data/search')
+@views.route('/api/data/<filename>/search')
+def search(filename='data'):
     """
     Search API endpoint that returns results from a JSON data file.
 
     Args:
+        filename (str): The filename of the JSON data file (default: 'data').
         q (str): The search query (keyword) to filter the results (default: None).
         field (str): The field(s) to search in, separated by commas. If not provided or empty string, all fields are searched (default: None).
         key (str): The key(s) to filter the results, separated by commas. If not provided or empty string, all items are returned (default: None).
@@ -104,11 +106,12 @@ def search():
     Raises:
         None
     """
-    filename = os.path.join(current_app.static_folder, 'data.json')
-    if not os.path.isfile(filename):
-        error_msg = 'Error: data file not found.'
+    filename = f'{filename}.json'
+    filepath = os.path.join(current_app.static_folder, filename)
+    if not os.path.isfile(filepath):
+        error_msg = f'Error: {filename} file not found.'
         return render_template('no-data.html', error_msg=error_msg)
-    with open(filename) as f:
+    with open(filepath) as f:
         data = json.load(f)
 
     search_query = request.args.get('q', '').strip().lower()
