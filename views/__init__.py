4a102aeb85f755f38496fb87a1efec451ffa6c91
Ryan Wolfslayer <xsltlibrary7@protonmail.com>
2023-03-22 11:49:16 -0400

    #2 - add exception if filename does not exist

--- views/__init__.py
+++ views/__init__.py
@@ -184,9 +184,8 @@ def data_table():
     # Get the filename parameter from the URL
     filename_param = request.args.get('filename')
 
-    if filename_param not in json_files:
-        error_msg = f'Error: could not find data file {filename_param}.'
-        return render_template('api-table.html', error_msg=error_msg)
+    if filename_param is None or filename_param not in json_files:
+        filename_param = json_files[0]
 
     filename = os.path.join(current_app.static_folder, filename_param)
 
@@ -211,6 +210,3 @@ def data_table():
     json_files = response.json()['files']
 
     return render_template('api-table.html', columns=columns, data_list=data_list, json_files=json_files, filename=filename_param)
-
-
-
